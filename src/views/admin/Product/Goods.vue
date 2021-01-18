14b21829b76923f42abeb5f348c37d7aa144c84e
XD邢丹 <8447293+xd-xingdan@user.noreply.gitee.com>
2021-01-18 11:29:17 +0800

    用户管理和权限管理a完整功能

---
+++ src/views/admin/Product/Goods.vue
@@ -0,0 +1,43 @@
+<template>
+ <div>
+商品列表
+ </div>
+</template>
+
+<script>
+export default {
+// 组件参数 接收来自父组件的数据
+ props: [],
+// 局部注册的组件
+ components: {},
+// 组件状态值
+ data() {
+ return {
+
+ };
+ },
+// 侦听器
+ watch:{},
+// 组件实例创建完成，属性已绑定，但DOM还未生成，$ el属性还不存在
+ created() {
+
+ },
+// el 被新创建的 vm.$ el 替换，并挂载到实例上去之后调用该钩子。
+// 如果 root 实例挂载了一个文档内元素，当 mounted 被调用时 vm.$ el 也在文档内。
+ mounted() {
+
+ },
+// 组件方法
+ methods: {
+
+ },
+// 计算属性
+ computed:{
+
+ }
+};
+</script>
+
+<style scoped>
+
+</style>
